c4b366a2d8d33f220766eb2e28e46ce997660479
Aditya Ranjan <136574821+Aditya-Ranjan1234@users.noreply.github.com>
2024-04-28 00:33:18 +0530

    Update and rename App.test.js to Header.jsx

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="Header">
+      <header>
+        <h1>Let's find some fun activity for you</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
